c9b3c4a5ecd721bf97eb7d8ddd29597d127eb8d5
m-ravshanovch <muhammadqodir.tr@gmail.com>
2026-07-13 11:22:42 +0500

    changes

--- src/layouts/client/ClientAuthLayout.vue
+++ src/layouts/client/ClientAuthLayout.vue
@@ -5,7 +5,7 @@
 <template>
     <div class="flex">
         <div class="hidden md:flex h-screen w-full  items-center justify-center bg-green-600">
-            <img src="/ishonchEat.png" class="h-full w-full object-cover" alt="">
+            <img src="/authLogo.png" class="h-full w-full object-cover" alt="">
         </div>
         <div class="w-full">
             <RouterView/>
